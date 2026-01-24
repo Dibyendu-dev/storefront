4d3f183f65fc583502e2dca23378ea752f3ad35e
Murtuza27 <Syedmurtuzamuteeb35@gmail.com>
2026-01-24 15:08:35 +0530

    implement filter data

--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from store.models import  Customer
 # Create your views here.
+
+def get_all_customers(request):
+    # customers = Customer.objects.all()
+    customers = Customer.objects.filter(membership=Customer.MEMBERSHIP_GOLD).order_by('first_name')
+    return render(request, 'customer.html', {'customers': customers})
