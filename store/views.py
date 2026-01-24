3ff530aab32ba5f66f8c9da846a7cee0efe1806d
Murtuza27 <ddas4548@gmail.com>
2026-01-24 15:11:23 +0530

    implement retriving all  data

--- store/views.py
+++ store/views.py
@@ -3,6 +3,6 @@ from store.models import  Customer
 # Create your views here.
 
 def get_all_customers(request):
-    # customers = Customer.objects.all()
-    customers = Customer.objects.filter(membership=Customer.MEMBERSHIP_GOLD).order_by('first_name')
-    return render(request, 'customer.html', {'customers': customers})
+     customers = Customer.objects.all()
+     # customers = Customer.objects.filter(membership=Customer.MEMBERSHIP_GOLD).order_by('first_name')
+     return render(request, 'customer.html', {'customers': customers})
